f1e03631922fa6527ccd0c5be00a7cdc25c4895b
Jirat Onaree <jirat.onaree@gmail.com>
2018-01-28 18:18:02 +0700

    Styling

--- components/IndexHeader.js
+++ components/IndexHeader.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default () => (
   <div>
-    <div className='text-center pb-2'>
+    <div className='text-center pb-4'>
       <Link href='/'>
         <h1>What's THAT COIN?</h1>
       </Link>
