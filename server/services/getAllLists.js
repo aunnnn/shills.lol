3143bc5d214c6dd2201d5f1e01b0a67195e82ae8
aunnnn <aun.wirawit@gmail.com>
2018-01-28 19:09:05 +0700

    Omit some fields

--- server/services/getAllLists.js
+++ server/services/getAllLists.js
@@ -2,7 +2,12 @@ const { send } = require('micro')
 const List = require('../models/List')
 
 const getAllLists = async (req, res) => {
-  const data = await List.find({})  
+  const data = await List.find({}, {
+    submitted_definitions: 0,
+    tags: 0,
+    _id: 0,
+    __v: 0
+  })
   send(res, 200, {
     all_lists: data
   })
