58ef74df8d6211f4204341bc9871140a83cc47ba
Jirat Onaree <jirat.onaree@gmail.com>
2018-01-31 11:20:32 +0700

    😫 <-> 😩

--- components/IndexHeader.js
+++ components/IndexHeader.js
@@ -1,20 +1,34 @@
+import { Component } from 'react'
 import Link from 'next/link'
 
-export default () => (
-  <div>
-    <div className='text-center py-4'>
-      <Link href='/'>
-        <h1>It takes too long to know the coin 😩</h1>
-      </Link>
-      <p>
-        <span>Can we TL;DR it?</span>
-        &nbsp;🤔
-      </p>
-    </div>
-    <style jsx>{`
-      h1 {
-        cursor: pointer;
-      }
-    `}</style>
-  </div>
-)
+export default class extends Component {
+  state = {
+    mouseOver: false
+  }
+
+  render () {
+    return (
+      <div>
+        <div className='text-center py-4'>
+          <Link href='/'>
+            <h1
+              onMouseEnter={() => this.setState({ mouseOver: true })}
+              onMouseLeave={() => this.setState({ mouseOver: false })}
+            >
+              It takes too long to know the coin {this.state.mouseOver ? '😫' : '😩'}
+            </h1>
+          </Link>
+          <p>
+            <span>Can we TL;DR it?</span>
+            &nbsp;🤔
+          </p>
+        </div>
+        <style jsx>{`
+          h1 {
+            cursor: pointer;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
