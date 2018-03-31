4683d41b7fd3c76c43d68ab23d08ea7b5f33c06a
Wirawit Rueopas <aun.wirawit@gmail.com>
2018-03-31 17:01:15 +0700

    Reduce input minimum number allowed

--- components/TldrInput.js
+++ components/TldrInput.js
@@ -6,7 +6,7 @@ import { addNewDefinition, addNewDefinitionLoading } from '../store'
 import APIService from '../utils/APIService'
 
 const MAXIMUM_CHARACTERS_ALLOWED = 77
-const MINIMUM_CHARACTERS_ALLOWED = 10
+const MINIMUM_CHARACTERS_ALLOWED = 8
 
 class TldrInput extends Component {
   state = {
