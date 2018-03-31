33743b46bf5704fc1180e3d3576de10a60a3a7d4
jeojoe <jirat.onaree@gmail.com>
2018-03-31 18:24:47 +0700

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,8 @@ export default ({ children }) => (
       <meta charset='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
       <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous' />
+      <link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
     </Head>
     <div className='py-3'>
       <div className='container'>
