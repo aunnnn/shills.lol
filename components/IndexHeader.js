fba9ecc475441a1454e8bef57d7383ae98e870f4
jeojoe <jirat.onaree@gmail.com>
2018-03-31 19:30:09 +0700

    Add logo lol

--- components/IndexHeader.js
+++ components/IndexHeader.js
@@ -11,12 +11,15 @@ export default class extends Component {
     return (
       <div className="bg d-flex px-3 py-1 align-items-center justify-content-between">
         <Link href='/'>
-          <h1
-            onMouseEnter={() => this.setState({ mouseOver: true })}
-            onMouseLeave={() => this.setState({ mouseOver: false })}
-          >
-            Coin Shills LOL
-          </h1>
+          <div className="d-flex align-items-center">
+            <img src="/static/lol.png" width="30" height="30" className="mr-1" />
+            <h1
+              onMouseEnter={() => this.setState({ mouseOver: true })}
+              onMouseLeave={() => this.setState({ mouseOver: false })}
+            >
+              Coin Shills LOL
+            </h1>
+          </div>
         </Link>
         <CoinsPicker />
         <style jsx>{`
